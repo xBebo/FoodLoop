6cfbae154bcdd8ebec2cc12017ffe4672aef9b36
Jana Sharaf <janasharaf634@gmail.com>
2026-09-20 12:13:21 +0300

    fix: stage remaining infrastructure dependency injection changes

--- src/FoodLoop.Infrastructure/DependencyInjection.cs
+++ src/FoodLoop.Infrastructure/DependencyInjection.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 namespace FoodLoop.Infrastructure;
+
 public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, string connectionString)
